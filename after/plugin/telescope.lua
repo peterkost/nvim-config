fcf95d0fc0bddf3ea5dce44e13a90fbaaa84e9a0
Peter Kostin <peter.kostin@ia.ca>
2023-08-04 11:24:01 -0400

    Updated hotkeys

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,3 +5,4 @@ vim.keymap.set('n', '<leader>ps', function()
   builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 vim.keymap.set('n', 'gr', builtin.lsp_references, { noremap = true, silent = true })
+vim.keymap.set('n', 'gd', builtin.lsp_definitions, { noremap = true, silent = true })
--- lua/kostin/packer.lua
+++ lua/kostin/packer.lua
@@ -32,7 +32,7 @@ return require('packer').startup(function(use)
     requires = {
       -- LSP Support
       { 'neovim/nvim-lspconfig' }, -- Required
-      {                        -- Optional
+      {                            -- Optional
         'williamboman/mason.nvim',
         run = function()
           pcall(vim.cmd, 'MasonUpdate')
@@ -41,9 +41,9 @@ return require('packer').startup(function(use)
       { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
       -- Autocompletion
-      { 'hrsh7th/nvim-cmp' }, -- Required
+      { 'hrsh7th/nvim-cmp' },     -- Required
       { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-      { 'L3MON4D3/LuaSnip' }, -- Required
+      { 'L3MON4D3/LuaSnip' },     -- Required
     }
   }
 end)
