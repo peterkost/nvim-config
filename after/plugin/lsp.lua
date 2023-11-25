018aa759753cfaeed30e86a67cc8f0a28a69496b
peterkost <46546031+peterkost@users.noreply.github.com>
2023-11-24 19:42:17 -0500

    Merging relevant changes from iA branch

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,10 +1,19 @@
 local lsp = require('lsp-zero').preset({})
 
 lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
+  lsp.default_keymaps({ buffer = bufnr })
 end)
 
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+vim.keymap.set("n", "<leader>ff", function() vim.lsp.buf.format() end)
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({ buffer = bufnr })
+  lsp.buffer_autoformat()
+end)
+
+vim.keymap.set("n", "<leader>e", function() vim.diagnostic.open_float() end)
+vim.keymap.set("n", "<leader>qf", function() vim.lsp.buf.code_action() end)
 
 lsp.setup()
