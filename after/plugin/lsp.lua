a215eef6257c90c93a740cc5d244c39786915bbc
peterkost <46546031+peterkost@users.noreply.github.com>
2024-02-22 08:48:54 -0500

    Add emmet

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,22 +1,67 @@
-local lsp = require('lsp-zero').preset({})
+local lsp = require("lsp-zero").preset({})
 
 lsp.on_attach(function(client, bufnr)
-    lsp.default_keymaps({ buffer = bufnr })
+	lsp.default_keymaps({ buffer = bufnr })
 end)
 
 -- (Optional) Configure lua language server for neovim
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-
+require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.on_attach(function(client, bufnr)
-    lsp.default_keymaps({ buffer = bufnr })
-    --lsp.buffer_autoformat()
+	lsp.default_keymaps({ buffer = bufnr })
+	--lsp.buffer_autoformat()
 end)
 
-vim.keymap.set("n", "<leader>e", function() vim.diagnostic.open_float() end)
-vim.keymap.set("n", "<leader>qf", function() vim.lsp.buf.code_action() end)
+local lspconfig = require("lspconfig")
+lspconfig.emmet_language_server.setup({
+	filetypes = {
+		"css",
+		"eruby",
+		"html",
+		"javascript",
+		"javascriptreact",
+		"less",
+		"sass",
+		"scss",
+		"pug",
+		"typescriptreact",
+	},
+	-- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
+	-- **Note:** only the options listed in the table are supported.
+	init_options = {
+		---@type table<string, string>
+		includeLanguages = {},
+		--- @type string[]
+		excludeLanguages = {},
+		--- @type string[]
+		extensionsPath = {},
+		--- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
+		preferences = {},
+		--- @type boolean Defaults to `true`
+		showAbbreviationSuggestions = true,
+		--- @type "always" | "never" Defaults to `"always"`
+		showExpandedAbbreviation = "always",
+		--- @type boolean Defaults to `false`
+		showSuggestionsAsSnippets = false,
+		--- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
+		syntaxProfiles = {},
+		--- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
+		variables = {},
+	},
+})
 
-vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end)
-vim.keymap.set("n", "<leader>ty", function() vim.lsp.buf.hover() end)
+vim.keymap.set("n", "<leader>e", function()
+	vim.diagnostic.open_float()
+end)
+vim.keymap.set("n", "<leader>qf", function()
+	vim.lsp.buf.code_action()
+end)
+
+vim.keymap.set("n", "<leader>rn", function()
+	vim.lsp.buf.rename()
+end)
+vim.keymap.set("n", "<leader>ty", function()
+	vim.lsp.buf.hover()
+end)
 
 lsp.setup()
