f22512dd1d7aa25d1f2ce8d9a432c3be02d7e807
Peter Kostin <Peter.Kostin@ibm.com>
2024-05-25 08:09:08 -0400

    feat: configure LSP for Python

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -13,6 +13,55 @@ lsp.on_attach(function(client, bufnr)
 end)
 
 local lspconfig = require("lspconfig")
+
+local configs = require("lspconfig/configs")
+local util = require("lspconfig/util")
+
+local path = util.path
+
+local function get_python_path(workspace)
+	-- Use activated virtualenv.
+	if vim.env.VIRTUAL_ENV then
+		return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
+	end
+
+	-- Find and use virtualenv in workspace directory.
+	for _, pattern in ipairs({ "*", ".*" }) do
+		local match = vim.fn.glob(path.join(workspace, pattern, "pyvenv.cfg"))
+		if match ~= "" then
+			return path.join(path.dirname(match), "bin", "python")
+		end
+	end
+
+	-- Fallback to system Python.
+	return exepath("python3") or exepath("python") or "python"
+end
+
+lspconfig.pyright.setup({
+	-- ...
+	before_init = function(_, config)
+		print("before")
+		config.settings.python.pythonPath = get_python_path(config.root_dir)
+	end,
+})
+
+-- lspconfig.pyright.setup({
+-- 	--on_attach = on_attach,
+-- 	settings = {
+-- 		pyright = { autoImportCompletion = true },
+-- 		python = {
+-- 			analysis = {
+-- 				autoSearchPaths = true,
+-- 				diagnosticMode = "openFilesOnly",
+-- 				useLibraryCodeForTypes = true,
+-- 				typeCheckingMode = "standard",
+-- 			},
+-- 		},
+-- 	},
+-- 	venvPath = ".",
+-- 	venv = "venv",
+-- })
+
 lspconfig.emmet_language_server.setup({
 	filetypes = {
 		"css",
